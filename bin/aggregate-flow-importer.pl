63d9a9ca6a798339c15d51a8e80a5f4f4a51334c
Dan Doyle <daldoyle@globalnoc.iu.edu>
2017-04-11 17:25:17 +0000

    updates to support aggregate pairs

--- bin/aggregate-flow-importer.pl
+++ bin/aggregate-flow-importer.pl
@@ -89,48 +89,60 @@ while ($start < $now){
 
         # Run through every elasticsearch result and crunch those numbers
         foreach my $result (@{$results->{'hits'}{'hits'}}){
-            
+
             my $metadata = $result->{'_source'}{'meta'};
             my $data     = $result->{'_source'}{'values'};
-            
-            my $num_packets = $data->{'num_packets'};
-            my $num_bits    = $data->{'num_bits'};
-            
+ 
             my $point_of_obs = $metadata->{'sensor_id'};            
 
-            # If this key was NOT a paired key, just make the single result
-            if (@pairs == 1){
-                my $key_val = $metadata->{$key};
+            # create records for each point of observation as well as a '*' 
+            # record indicating the summazation of each                    
+            foreach my $point ( ($point_of_obs, '*') ){
                 
-                if (! $key_val){
-                    print "Unable to find value for $key in record, skipping\n";
-                    next;
+                # If this key was NOT a paired key, just make the single result
+                if (@pairs == 1){
+                    my $key_val = $metadata->{$key};
+                    
+                    if (! $key_val){
+                        print "Unable to find value for $key in record, skipping\n";
+                        next;
+                    }
+                    
+                    if (! exists $crunched_singles{$point}{$key_val}){
+                        $crunched_singles{$point}{$key_val} = {};
+                    }
+                    my $key_data = $crunched_singles{$point}{$key_val};
+                    
+                    update_key_data($data, $key_data, "");
                 }
-                
-                $crunched_singles{$point_of_obs}{$key_val}{'bits'} += $num_bits;
-                
-                # We also want to include a "*" point of observation that represents the totality
-                # of each flow on every sensor. There may be duplication of traffic here.
-                $crunched_singles{'*'}{$key_val}{'bits'} += $num_bits;
+            
+                # If this key WAS a paired key, make the paired result
+                else {
+                    my $key_val_a = $metadata->{$pairs[0]};                
+                    my $key_val_b = $metadata->{$pairs[1]};
+                    
+                    if (! $key_val_a || ! $key_val_b){
+                        print "Unable to find values for either $pairs[0] or $pairs[1] in record, skipping\n";
+                        next;
+                    }
+                                 
+                    if (! exists $crunched_pairs{$point}{$key_val_a}{$key_val_b}){
+                        $crunched_pairs{$point}{$key_val_a}{$key_val_b} = {};
+                    }
+                    if (! exists $crunched_pairs{$point}{$key_val_b}{$key_val_a}){
+                        $crunched_pairs{$point}{$key_val_b}{$key_val_a} = {};
+                    }
+                    
+                    my $key_data_output = $crunched_pairs{$point}{$key_val_a}{$key_val_b};
+                    my $key_data_input  = $crunched_pairs{$point}{$key_val_b}{$key_val_a};
+                    
+                    update_key_data($data, $key_data_output, "output_");
+                    update_key_data($data, $key_data_input, "input_");
+                }                   
             }
-            # If this key WAS a paired key, make the paired result
-            else {
-                my $key_val_a = $metadata->{$pairs[0]};
-                my $key_val_b = $metadata->{$pairs[1]};
+        }
 
-                if (! $key_val_a || ! $key_val_b){
-                    print "Unable to find values for either $pairs[0] or $pairs[1] in record, skipping\n";
-                    next;
-                }
 
-                $crunched_pairs{$point_of_obs}{$key_val_a}{$key_val_b}{'output'} += $num_bits;
-                $crunched_pairs{$point_of_obs}{$key_val_b}{$key_val_a}{'input'} += $num_bits;
-
-                $crunched_pairs{'*'}{$key_val_a}{$key_val_b}{'output'} += $num_bits;
-                $crunched_pairs{'*'}{$key_val_b}{$key_val_a}{'input'} += $num_bits;
-            }
-        }    
-    
         my @to_send;
         my @meta_send;        
 
@@ -140,71 +152,125 @@ while ($start < $now){
         # TSDS, but is harmless if they haven't been, just won't add any data
         foreach my $point_of_obs (keys %crunched_singles){
             
-            my $tsds_type = "netflow_" . $key;
-            
+            my $tsds_type = "netflow_" . $key;            
             my $data = $crunched_singles{$point_of_obs};
             
             foreach my $meta_val (keys %$data){
-                my $bits = 0 + ($data->{$meta_val}{'bits'} || 0);
+                my $send_values = {};
+
+                foreach my $val_name (keys %{$data->{$meta_val}}){
+
+                    my $val = $data->{$meta_val}->{$val_name};
+                    
+                    # avg_rtt for example is an array, need to crunch to a single value now
+                    if (ref $val){
+                        my $total = 0;
+                        foreach my $sub_val (@$val){
+                            $total += $sub_val if defined $sub_val;
+                        }
+                        $val = $total / @$val;
+                    }
+
+                    $val = 0 + $val if defined $val;
+                    $send_values->{$val_name} = $val;
+                }
+
+                push(@to_send,
+                     {
+                         interval => 86400,
+                         type => $tsds_type,
+                         time => $submit_time,
+                         meta => {
+                             $key => $meta_val,                    
+                             "point_of_observation" => $point_of_obs
+                         },
+                         values => $send_values
+                     }
+                    );
                 
-                push(@to_send, {
-                    interval => 86400,
-                    type => $tsds_type,
-                    time => $submit_time,
-                    meta => {
-                        $key => $meta_val,                    
-                        "point_of_observation" => $point_of_obs
-                    },
-                    values => {
-                        bits => $bits
-                    }            
-                     });
-                
-                push(@meta_send, {
-                    type => $tsds_type,
-                    start => $submit_time,
-                    end => $submit_time + 86400,
-                    $key => $meta_val,
-                    "point_of_observation" => $point_of_obs
-                     });            
+                push(@meta_send,
+                     {
+                         type  => $tsds_type,
+                         start => $submit_time,
+                         end   => $submit_time + 86400,
+                         $key  => $meta_val,
+                         "point_of_observation" => $point_of_obs
+                     }
+                    );            
             }        
         }
 
         # Same thing but for any paired aggregates
         foreach my $point_of_obs (keys %crunched_pairs){
             
-            my $tsds_type = "netflow_" . $pairs[0] . "_" . $pairs[1];
-            
+            my $tsds_type = "netflow_" . $pairs[0] . "_" . $pairs[1];            
             my $data = $crunched_pairs{$point_of_obs};          
             
             foreach my $meta_val_a (keys %$data){
                 foreach my $meta_val_b (keys %{$data->{$meta_val_a}}){
-                    my $input = 0 + ($data->{$meta_val_a}->{$meta_val_b}->{'input'} || 0);
-                    my $output = 0 + ($data->{$meta_val_a}->{$meta_val_b}->{'output'} || 0);
+
+                    my $send_values = {};
+
+                    # Copy over all the observed values from the crunched data into our TSDS message
+                    foreach my $val_name (keys %{$data->{$meta_val_a}->{$meta_val_b}}){
+
+                        my $val = $data->{$meta_val_a}->{$meta_val_b}->{$val_name};
+
+                        # avg_rtt for example is an array, need to crunch to a single average 
+                        # value now
+                        if (ref $val){
+                            my $total = 0;
+                            foreach my $sub_val (@$val){
+                                $total += $sub_val if defined $sub_val;
+                            }
+                            $val = $total / @$val;
+                        }
+
+                        $val = 0 + $val if defined $val;
+                        $send_values->{$val_name} = $val;
+                    }
+
+                    # This is sort of a hack - go through each and for every input_* or output_*
+                    # if we don't see the inverse create it as an null record for completeness
+                    foreach my $send_val (keys %$send_values){
+                        my $other;
+                        if ($send_val =~ /^output_(.+)/){
+                            $other = "input_$1"; 
+                        }
+                        if ($send_val =~ /^input_(.+)/){
+                            $other = "output_$1"; 
+                        }
+                        die "Unable to parse key $send_val for pair check" if (! $other);
+                        if (! exists $send_values->{$other}){
+                            $send_values->{$other} = undef;
+                        }
+                    }
                 
-                    push(@to_send, {
-                        interval => 86400,
-                        type => $tsds_type,
-                        time => $submit_time,
-                        meta => {
-                            $pairs[0] => $meta_val_a,                    
-                            $pairs[1] => $meta_val_b,                    
-                            "point_of_observation" => $point_of_obs
-                        },
-                        values => {
-                            input => $input,
-                            output => $output
-                        }            
-                     });
-                
-                    push(@meta_send, {
-                        type => $tsds_type,
-                        start => $submit_time,
-                        end => $submit_time + 86400,
-                        $pairs[0] => $meta_val_a,
-                        $pairs[1] => $meta_val_b,
-                        "point_of_observation" => $point_of_obs
-                         });            
+                    push(@to_send, 
+                         {
+                             interval => 86400,
+                             type => $tsds_type,
+                             time => $submit_time,
+                             meta => {
+                                 $pairs[0] => $meta_val_a,                    
+                                 $pairs[1] => $meta_val_b,                    
+                                 "point_of_observation" => $point_of_obs
+                             },
+                             values => $send_values                        
+                         }
+                        );
+                    
+                    push(@meta_send, 
+                          {
+                              type => $tsds_type,
+                              start => $submit_time,
+                              end => $submit_time + 86400 - 1,
+                              $pairs[0] => $meta_val_a,
+                              $pairs[1] => $meta_val_b,
+                              "point_of_observation" => $point_of_obs
+                          }
+                         );
+                    
                 }        
             }
         }
@@ -226,7 +292,7 @@ while ($start < $now){
      
         # This is so hacky since add_data is asynch, needs to be enhanced.
         # Gives add_data time to finish on the backend
-        sleep(10);
+        sleep(5);
         
 
         # Now that data has been added, signal end of the metadata record
@@ -244,4 +310,45 @@ while ($start < $now){
             }
         }
     }
+}
+
+
+sub update_key_data {
+    my $data        = shift;
+    my $agg_data    = shift;
+    my $hash_prefix = shift;
+
+            
+    my $num_bits    = $data->{'num_bits'};
+    my $num_packets = $data->{'num_packets'};
+    my $bps         = $data->{'bits_per_second'};
+    my $pps         = $data->{'packets_per_second'};
+    my $duration    = $data->{'duration'};
+    my $max_rtt     = $data->{'tcp_rtt_max'};
+    my $avg_rtt     = $data->{'tcp_rtt_avg'};
+    my $min_rtt     = $data->{'tcp_rtt_min'};
+    my $loss        = undef;
+    
+    if (exists $data->{'tcp_rexmit_pkts'} && defined $data->{'tcp_rexmit_pkts'}){
+        $loss = $data->{'tcp_rexmit_pkts'} / $num_packets;
+    }
+
+    $agg_data->{$hash_prefix . 'bits'} += $num_bits if (defined $num_bits);
+    $agg_data->{$hash_prefix . 'packets'} += $num_packets if (defined $num_packets);
+    $agg_data->{$hash_prefix . 'bps'} += $bps if (defined $bps);
+    $agg_data->{$hash_prefix . 'pps'} += $pps if (defined $pps);
+    $agg_data->{$hash_prefix . 'duration'} += $duration if (defined $duration);
+    $agg_data->{$hash_prefix . 'loss'} += $loss if (defined $loss);
+
+    my $existing_max_rtt = $agg_data->{$hash_prefix . "max_rtt"};
+    if (defined $max_rtt && (! defined $existing_max_rtt || $max_rtt > $existing_max_rtt)){
+        $agg_data->{$hash_prefix . 'max_rtt'} = $max_rtt;
+    }
+
+    my $existing_min_rtt = $agg_data->{$hash_prefix . "min_rtt"};
+    if (defined $min_rtt && (! defined $existing_min_rtt || $min_rtt < $existing_min_rtt)){
+        $agg_data->{$hash_prefix . "min_rtt"} = $min_rtt;
+    }
+    
+    push(@{$agg_data->{$hash_prefix . 'avg_rtt'}}, $avg_rtt);
 }
